2a30f4ea5f5080ac1634af6f62ed38daee0ad331
viniciusfasam123 <viniciusfasam123@gmail.com>
2020-06-26 16:41:03 -0300

    Versão Adicionar Empregado-Empresa

--- frontend/src/app/empresa-detalhes/empresa-detalhes.component.ts
+++ frontend/src/app/empresa-detalhes/empresa-detalhes.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Empresa } from '../empresa';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EmpresaService } from '../empresa.service';
+import { Empregado } from "../empregado";
+import { Observable } from "rxjs";
+import { EmpregadoServico } from "../empregado.service";
 
 @Component({
   selector: 'app-empresa-detalhes',
@@ -10,26 +13,37 @@ import { EmpresaService } from '../empresa.service';
 })
 export class EmpresaDetalhesComponent implements OnInit {
 
+  empregados: Observable<Empregado[]>;
   id: number;
   empresa: Empresa;
+  empregadosEmpresa: Observable<Empresa[]>
 
   constructor(private route: ActivatedRoute, private router: Router,
-    private empresaService: EmpresaService) { }
+    private empresaService: EmpresaService,
+    private empregadoServico: EmpregadoServico) { }
 
   ngOnInit() {
     this.empresa = new Empresa();
 
     this.id = this.route.snapshot.params['id']
 
+    this.empregados = this.empregadoServico.getEmpregadoLista();
+    console.log(this.empregados)
     this.empresaService.getEmpresa(this.id)
     .subscribe(data => {
       console.log(data)
       this.empresa = data;
     }, error => console.log(error));
+
+
   }
 
   listar(){
     this.router.navigate(['empresas'])
   }
 
+  adicionaEmpregado(id: number){
+
+    this.empregadosEmpresa= this.empresaService.adicionaEmpregado(id,this.empresa.id);
+  }
 }
--- frontend/src/app/empresa.service.ts
+++ frontend/src/app/empresa.service.ts
@@ -1,34 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
 export class EmpresaService {
-
   private baseUrl= 'http://localhost:8080/springboot-crud-rest/api/v1/empresas';
-
-
   constructor(private http: HttpClient) { }
-
   getEmpresa(id: number): Observable<any>{
     return this.http.get(`${this.baseUrl}/${id}`)
   }
-
   criarEmpresa(id: string, empresa: object): Observable<Object>{
     return this.http.post(`${this.baseUrl}/${id}`, empresa); 
   }
-
   atualizarEmpresa(id: number, value: any): Observable<Object>{
     return this.http.put(`${this.baseUrl}/${id}`, value);
   }
-
   excluirEmpresa(id: number): Observable<any>{
     return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text'})
   }
-
   getEmpresaLista(): Observable<any> {
     return this.http.get(`${this.baseUrl}`);
+  }
+
+  adicionaEmpregado(id: number, idEmpresa: number): Observable<any>{
+    return this.http.post(`${this.baseUrl+'/adicionarempregado'}/${id}`, idEmpresa)
   }
 }
